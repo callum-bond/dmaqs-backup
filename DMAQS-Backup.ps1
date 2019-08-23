fa1c6764c38ef1821437352c780827529e9016ee
Callum Bond <callumbond199@gmail.com>
2019-08-23 19:50:47 +1000

    fixed some things

--- DMAQS-Backup.ps1
+++ DMAQS-Backup.ps1
@@ -9,6 +9,10 @@
   Creation Date:  19/08/2019
   Purpose/Change: Initial script development
 
+  # TODO:
+  - Fix up logging
+  - Test if uploads to S3
+  - Confirm correct calling of batch scripts
 #>
 
 #---------------------------------------------------------[Initialisations]--------------------------------------------------------
@@ -19,26 +23,15 @@ $ErrorActionPreference = "Stop"
 # Set proxy address
 # Set-VpnConnectionProxy = "" 
 
-
-# Database connection credentials
-# TODO: Find a better way of authenticating DB access (i.e. not plaintext)
-# TODO: Execute remote SQL file, incoporating postgre SQL exit flag
-
-# $dbServer = ""
-# $dbName = ""
-# $uid = ""
-# $pwd = ""
-
-
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
 
 # Name and location of logs
-$logs = "D:\Logs"
-$logFile = New-Item -ItemType File -Path "$logs\$((Get-Date).ToString('yyyyMMdd'))"
+# $logs = "C:\Temp\Logs"
+# $logFile = New-Item -ItemType Directory -Path "$logs\Log_$((Get-Date).ToString('yyyyMMdd'))"
 
 # File locations
-$src = "D:\Extract"
-$dest = "C:\Extract_Archive"
+$src = "C:\Temp\Extract"
+$dest = "C:\Temp\Extract_Archive"
 
 # $out = C:Pathfile.bat
 
@@ -47,15 +40,15 @@ $dest = "C:\Extract_Archive"
 
 Function stopDMAQS {
 # Stop DMAQS service once safe to do so
-    if ((Get-ChildItem $src -Force | Select-Object -First 1 |Measure-Object).Count -eq 0) {
+    if ((Get-ChildItem $src -Force | Select-Object -First 1 | Measure-Object).Count -eq 0) {
         # Stop-Process -Name "DMAQS"
-        Write-Output "DMAQS stopped..."
-        log-Write "DMAQS stopped."
+        Write-Output "DMAQS stopped."
+        # logWrite "DMAQS stopped."
 } Else {
         Write-Output "DMAQS service cannot be stopped at this time. Retrying in 30 seconds."
         Start-Sleep -Seconds 30
         stopDMAQS
-        log-Write "DMAQS failed to stop... Retrying in 30 seconds."
+        # logWrite "DMAQS failed to stop... Retrying in 30 seconds."
         }    
 }
 
@@ -63,21 +56,32 @@ Function queryDMAQS {
 # Query DMAQS database.
 # Version 1: Call existing batch script and pipe output to $out variable.
     Try {
-        D:/Util/extract_cars.bat
+        # D:/Util/extract_cars.bat
+        C:/Temp/Scripts/test_create.bat
         Write-Output "Extracted DMAQS data to D:/Extract."
-        logWrite "Extracted DMAQS data to D:/Extract."
+        # logWrite "Extracted DMAQS data to D:/Extract."
     }
     
     Catch {
         Write-Output "Query failed."
-        logWrite "Query failed."
-        emailAlert
+        # logWrite "Query failed."
+        # emailAlert
         Exit
     }
 }
 
 Function truncateData {
-    # D:/Util/truncate_vlocity.log.bat
+    Try {
+    # D:/Util/truncate_vlocity_log.bat
+    C:/Temp/Scripts/test_rename.bat
+    Write-Output "Logs truncated."
+    # log
+}
+
+    Catch {
+    Write-Output "Something failed."
+    Exit
+    }
 }
 
 Function uploadToS3 {
@@ -92,30 +96,29 @@ Function uploadToS3 {
         Foreach ($f in "*.csv.gz") {
         Write-S3Object -BucketName Vlocity_Incoming_Logs_Test -File $f -Key $f -CannedACLName public-read
         Write-Output "Uploaded $f."
-        logWrite "Uploaded $f."
+        # logWrite "Uploaded $f."
     }
         } Catch {
         Write-Output "File $f failed to upload."
-        logWrite "File $f failed to upload."
-        emailAlert
+        # logWrite "File $f failed to upload."
+        # emailAlert
     }
 }
 
 Function archiveData {
-    # Create archive of data and store in D:/Archive
+    # Create time-stamped folder and echo uploaded logs there
     <#
-    TODO: Move logs to newly created folder
+    # TODO: Find nicer way of detecting newly created folder and moving logs there
     #>
-
     Try {
-        New-Item -ItemType Directory -Path "$src\Uploaded_To_S3\$((Get-Date).ToString('yyyyMMdd'))"
-        # Move logs to folder
-        Write-Output "Logs moved to [folder]". 
-        logWrite "Logs moved to folder." 
+        $New_Dest = New-Item -ItemType Directory -Path "$dest\Uploaded_To_S3_$((Get-Date).ToString('yyyyMMdd'))"
+        Get-ChildItem -Path $src -Recurse -Include *.txt | Move-Item -Force -Destination $New_Dest
+        Write-Output "Logs moved to D:/Extract_Archive."
+        # logWrite "Logs moved to folder." 
     } Catch {
         Write-Output "Failed to move item to archive folder."
-        logWrite "Failed to move item to archive folder."
-        emailAlert
+        # logWrite "Failed to move item to archive folder."
+        # emailAlert
     }
 }
 
@@ -129,16 +132,20 @@ Function startDMAQS {
     If ((Get-ChildItem $src -Force | Select-Object -First 1 |Measure-Object).Count -eq 0) {
         # Start-Process -Name "DMAQS"
         Write-Output "DMAQS service started."
-        logWrite "DMAQS service started."
+        # logWrite "DMAQS service started."
         Exit
     } Else {
     # Fix this up
-        emailAlert
+        # emailAlert
         Exit
     }
 }
 
 Function emailAlert($emailTo) {
+# Send email to stakeholders if unexpected error encountered
+<#
+# TODO: Find way to authenticate with smtp server
+#>
     $message = @"
         Attention!
 
@@ -150,18 +157,23 @@ Function emailAlert($emailTo) {
 "@        
  
     $emailFrom = "Callum.Bond@kineticit.com.au"
+    $emailTo = "Callumbond199@gmail.com"
     $subject="Alert - DMAQ Backup Failed"
-    $smtpserver="# I have no idea what the details for this are #"
+    $smtpserver="smtp-mail.outlook.com"
     $smtp=New-Object Net.Mail.SmtpClient($smtpServer) 
     $smtp.Send($emailFrom, $emailTo, $subject, $message) 
 } 
 
+# Fix this
 
+<# 
 Function logWrite {
     # Write to log 
     Param([string]]$logString])
     Add-Content $logFile -value $logstring
     }
+
+#>
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
@@ -170,15 +182,15 @@ Function logWrite {
 
 # Call functions
 
-# stopDMAQS
+stopDMAQS
 
-# queryDMAQS
+queryDMAQS
 
-# truncateData
+truncateData
 
 # uploadToS3
 
-# archiveData
+archiveData
 
-# startDMAQS
+startDMAQS
 
